3c5508300e1c9cbe15ba3b14fb6aa11e277ac4f6
Hulkar Sharif <hulkar.sharif@gmail.com>
2024-05-25 17:59:45 -0400

    Refactor the Box component

--- src/App.js
+++ src/App.js
@@ -53,14 +53,24 @@ export default function App() {
                 <NumResults movies={movies} />
             </Navbar>
             <Main>
-                <Box>
+                <Box element={<MovieList movies={movies} />} />
+                <Box
+                    element={
+                        <>
+                            {" "}
+                            <WatchedSummary watched={watched} />
+                            <WatchedMovieList watched={watched} />
+                        </>
+                    }
+                />
+                {/* <Box>
                     <MovieList movies={movies} />
                 </Box>
 
                 <Box>
                     <WatchedSummary watched={watched} />
                     <WatchedMovieList watched={watched} />
-                </Box>
+                </Box> */}
             </Main>
         </>
     );
@@ -113,7 +123,7 @@ function Main({ children }) {
     return <main className="main">{children}</main>;
 }
 
-function Box({ children }) {
+function Box({ element }) {
     const [isOpen, setIsOpen] = useState(true);
 
     return (
@@ -124,7 +134,7 @@ function Box({ children }) {
             >
                 {isOpen ? "–" : "+"}
             </button>
-            {isOpen && children}
+            {isOpen && element}
         </div>
     );
 }
