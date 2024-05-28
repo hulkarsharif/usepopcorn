627e7f0651f4374a7c24580108980db954e7871c
Hulkar Sharif <hulkar.sharif@gmail.com>
2024-05-28 14:39:33 -0400

    Create star for rating

--- src/StarRating.js
+++ src/StarRating.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const containerStyle = {
     display: "flex",
     alignItems: "center",
@@ -5,8 +7,7 @@ const containerStyle = {
 };
 
 const starContainerStyle = {
-    display: "flex",
-    gap: "4px"
+    display: "flex"
 };
 
 const textStyle = {
@@ -14,14 +15,23 @@ const textStyle = {
     margin: "0"
 };
 export default function StarRating({ maxRating }) {
+    const [rating, setRating] = useState(1);
+
+    function handleRating(rating) {
+        setRating(rating);
+    }
     return (
         <div style={containerStyle}>
             <div style={starContainerStyle}>
                 {Array.from({ length: maxRating }, (_, i) => (
-                    <Star key={i} />
+                    <Star
+                        key={i}
+                        onRate={() => setRating(i + 1)}
+                        full={rating >= i + 1}
+                    />
                 ))}
             </div>
-            <p style={textStyle}>10</p>
+            <p style={textStyle}>{rating || ""}</p>
         </div>
     );
 }
@@ -33,26 +43,41 @@ const starStyle = {
     cursor: "pointer"
 };
 
-function Star() {
+function Star({ onRate, full }) {
     return (
-        <span role="button" style={starStyle}>
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="#000"
-            >
-                <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="{2}"
-                    d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
-                />
-            </svg>
+        <span role="button" style={starStyle} onClick={onRate}>
+            {full ? (
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="#000"
+                >
+                    <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2} // Corrected attribute
+                        d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
+                    />
+                </svg>
+            ) : (
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="#000"
+                >
+                    <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2} // Corrected attribute
+                        d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
+                    />
+                </svg>
+            )}
         </span>
     );
 }
-
 /*
 FULL STAR
 
@@ -70,18 +95,6 @@ FULL STAR
 
 EMPTY STAR
 
-<svg
-  xmlns="http://www.w3.org/2000/svg"
-  fill="none"
-  viewBox="0 0 24 24"
-  stroke="#000"
->
-  <path
-    strokeLinecap="round"
-    strokeLinejoin="round"
-    strokeWidth="{2}"
-    d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
-  />
-</svg>
+
 
 */
